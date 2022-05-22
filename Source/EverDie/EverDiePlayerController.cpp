fa3be399e50576b9f3de9d2717620e2601260268
Anatoly <zeyatolyas@gmail.com>
2022-05-22 21:37:37 +0300

    new pixel icons

--- Source/EverDie/EverDiePlayerController.cpp
+++ Source/EverDie/EverDiePlayerController.cpp
@@ -5,6 +5,8 @@
 #include "EverDiePlayer.h"
 #include "GameFramework/PlayerInput.h"
 
+//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%s"), *MoveToLocation.ToString()));
+
 AEverDiePlayerController::AEverDiePlayerController(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
@@ -17,29 +19,23 @@ void AEverDiePlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	ControlledPlayer = Cast<AEverDiePlayer>(GetPawn());
-
-	//SetInputMode(FInputModeGameAndUI());
 }
 
 void AEverDiePlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%s"), *ControlledPlayer->GetName())); //working debug message
+
 	if (CursorPressed)
 	{
 		FHitResult CursorHit;
 		GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
 		MoveToLocation = CursorHit.Location - ControlledPlayer->GetActorLocation();
 		MoveToLocation.Y = 0;
-		//MoveToLocation.X /= MoveToLocation.Size();
-		//MoveToLocation.Z /= MoveToLocation.Size();
 		MoveToLocation.Normalize();
 
 		ControlledPlayer->AddActorWorldOffset(MoveToLocation * MovementSpeed);
 		float angle = FMath::Atan2(MoveToLocation.X, MoveToLocation.Z);
 		CheckCharDirection(angle);
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%s"), *MoveToLocation.ToString()));
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%f"), angle));
 		CharDirection = CharDirections::None;
 	}
 }
@@ -47,11 +43,8 @@ void AEverDiePlayerController::Tick(float DeltaTime)
 void AEverDiePlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-	//UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Action",
-	//	EKeys::SpaceBar));
 	InputComponent->BindAction("MoveTo", IE_Pressed, this, &AEverDiePlayerController::MoveToCursorPressed);
 	InputComponent->BindAction("MoveTo", IE_Released, this, &AEverDiePlayerController::MoveToCursorReleased);
-	//InputComponent->BindAction("MoveTo", IE_Pressed, this, &AEverDiePlayerController::SomeTestAction);
 	InputComponent->BindAction("Action", IE_Pressed, this, &AEverDiePlayerController::SomeTestAction);
 }
 
